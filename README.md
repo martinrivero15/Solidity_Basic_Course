997021b18d7f385686d3f30063ab06635211f3ee
martinrivero15 <martinnicolasrivero@gmail.com>
2024-10-13 13:24:53 -0300

    hola_mundo

---
+++ README.md
@@ -0,0 +1,12 @@
+# Solidity_Basic_Course
+This is a solidity course to start from scratch.
+
+## This is a commit from Remix
+
+This a change from IDE *Remix*, **web version**
+
+
+## This is a commit from GitHub
+
+This a change from *GitHub*, **web version**
+This is possible from GitHub
---
+++ hola_mundo.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+//0x54546AdD45B63bAe8b5E809DC5b82EDa8258252d
+
+pragma solidity  0.8.24; // definición del compilador
+
+//logica del contrato
+
+contract hola_mundo {
+    
+    string public saludo = "Hola desde Argentina"; // declaración de contenido de variable "saludo"
+
+}
+
+//smart contract address 0x54546AdD45B63bAe8b5E809DC5b82EDa8258252d
+// Arbitrum Sepolia https://sepolia.arbiscan.io/
